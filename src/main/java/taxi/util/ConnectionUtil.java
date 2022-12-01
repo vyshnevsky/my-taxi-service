f0b0af5045d37615ad027e0bcbc8fa8dc1938a77
Bohdan Vyshnevskyy <yksvenhsiv@gmail.com>
2022-12-01 14:02:42 +0100

    solution 4.2

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL
-            = "jdbc:mysql://localhost:3306/taxi_db?useUnicode=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "mersedes241";
+    private static final String URL = "YOUR_URL";
+    private static final String USERNAME = "YOUR_USERNAME";
+    private static final String PASSWORD = "YOUR_PASSWORD";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
